2186f388162fd3b5db9a2e9d48fd8fbd2038eee8
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-09 23:17:36 +0900

    main.jsの中身を表示できた

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
+var items = [
+   { "text": "1st Post" },
+   { "text": "2nd Post" }
+];
+
+
+
+
+//index.ejsを指定する。
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Entry list", items: items });
 };
